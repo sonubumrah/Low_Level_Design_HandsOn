7ef78a72ece9a4f4ada533be91e60a7d19ad5e96
Sonu Yadav <your-email@example.com>
2026-09-21 23:23:33 +0530

    implemented elevator system

--- src/ElevatorSystem/OutdoorButton.java
+++ src/ElevatorSystem/OutdoorButton.java
@@ -1,5 +1,22 @@
 package ElevatorSystem;
 
 public class OutdoorButton implements Button{
+    private char buttonNumber;
 
+    public OutdoorButton(char buttonNumber) {
+        this.buttonNumber = buttonNumber;
+    }
+    public char getButtonNumber() {
+        return buttonNumber;
+    }
+    public void setButtonNumber(char buttonNumber) {
+        this.buttonNumber = buttonNumber;
+    }
+
+
+    @Override
+    public void press() {
+        System.out.println("Outdoor button " + buttonNumber + " pressed.");
+
+    }
 }
